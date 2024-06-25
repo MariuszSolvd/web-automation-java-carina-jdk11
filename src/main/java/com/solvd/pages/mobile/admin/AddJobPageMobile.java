bd16f67dc770ac6d69a09bf8de0c9c9967bd4f2b
MariuszSolvd <mkaminski@solvd.com>
2024-06-25 13:07:08 +0200

    Fixed test 5 for android mobile

---
+++ src/main/java/com/solvd/pages/mobile/admin/AddJobPageMobile.java
@@ -0,0 +1,15 @@
+package com.solvd.pages.mobile.admin;
+
+
+import com.solvd.pages.common.admin.AddJobPageBase;
+import com.zebrunner.carina.utils.factory.DeviceType;
+import org.openqa.selenium.WebDriver;
+
+@DeviceType(pageType = DeviceType.Type.ANDROID_PHONE, parentClass = AddJobPageBase.class)
+public class AddJobPageMobile extends AddJobPageBase {
+
+    public AddJobPageMobile(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+}
